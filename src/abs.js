08c5c919514d6d05186f606e367151114a29a2af
Kirill Peshin <kirill_peshin@epam.com>
2021-09-10 10:43:15 +0300

    01

---
+++ src/abs.js
@@ -0,0 +1,9 @@
+for (var i = 0; i < 3; i++) {}  // Empty on purpose or missing piece of code ?
+a = 1;
+a = 3
+if (a > 3) {
+    a = 4;
+} if (a < 3) {
+    a = 2;
+}
+module.exports = num => num < 0 ? num * -1 : num;
